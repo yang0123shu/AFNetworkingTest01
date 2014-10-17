b81e5f2ef6f5dd6f67ef618a71531830ad4ad867
yang0123shu <yang0123shu@163.com>
2014-10-17 17:00:21 +0800

    1
    1

--- AFNetworkingTest01/DLViewController.m
+++ AFNetworkingTest01/DLViewController.m
@@ -7,16 +7,30 @@
 //
 
 #import "DLViewController.h"
-
+#import "AFHTTPRequestOperationManager.h"
 @interface DLViewController ()
 
+@property (nonatomic,strong) AFHTTPRequestOperationManager * AFMgr;
+@property (nonatomic,strong) AFHTTPResponseSerializer * responseSerzier;
 @end
 
 @implementation DLViewController
+- (IBAction)toGET:(id)sender {
+    //AFNetworking 发送GET请求
+    [self.AFMgr GET:@"http://yl.cms.palmtrends.com/api_v2.php?action=lastest_list&sa=new&offset=0&count=15&uid=8507468&platform=a&mobile=LT22i&pid=10048&e=94692847a934e49327c8cd50b7f9731e" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"%@",responseObject);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"%@",error);
+    }];
+}
+- (IBAction)toDownload:(id)sender {
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.AFMgr=[AFHTTPRequestOperationManager manager];
+    self.responseSerzier=[[AFHTTPResponseSerializer alloc]init];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
